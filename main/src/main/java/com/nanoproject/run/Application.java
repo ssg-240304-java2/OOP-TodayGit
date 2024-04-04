d9a6616b39750646e16f7c63d7f377c1c5801a5a
lajaha9887 <lajaha99@koreatech.ac.kr>
2024-04-04 18:40:56 +0900

    폴더 수정

--- main/src/main/java/com/nanoproject/run/Application.java
+++ main/src/main/java/com/nanoproject/run/Application.java
@@ -1,14 +1,22 @@
 package com.nanoproject.run;
 
+import com.nanoproject.controller.JoinMembership;
 import com.nanoproject.model.dto.member.MemberDTO;
-import com.nanoproject.view.Login;
+import com.nanoproject.view.LoginMenu;
 import com.nanoproject.view.Menu;
 
 
 public class Application {
     public static void main(String[] args) {
-        Login loginMenu = new Login();
-        MemberDTO member = loginMenu.menu();
-        new Menu(member).run();
+        JoinMembership joinMembership = new JoinMembership();
+        MemberDTO memberDTO = new MemberDTO();
+        while (true) {
+            LoginMenu loginMenu = new LoginMenu(joinMembership);
+            memberDTO = loginMenu.menu();  // 로그인창
+            if(loginMenu.getNum() == 1){
+                break;
+            }
+            new Menu(memberDTO).run(); // 구매창
+        }
     }
 }
